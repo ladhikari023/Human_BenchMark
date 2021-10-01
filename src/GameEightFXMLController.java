0e712d352cc622fbf5a23c3f261c908d6b948dde
Laxman Adhikari <ladhikari023@unm.edu>
2021-09-30 23:48:47 -0600

    Game Eight => Typing game Committed

--- src/GameEightFXMLController.java
+++ src/GameEightFXMLController.java
@@ -1,2 +1,25 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
 public class GameEightFXMLController {
+    @FXML
+    private TextArea gEightTextArea;
+
+    public void initialize(){
+        gEightTextArea.setWrapText(true);
+        gEightTextArea.setEditable(false);
+        gEightTextArea.setText("Typing words come here");
+    }
+
+    public void startGameEight(MouseEvent mouseEvent) {
+        //game logic goes here
+    }
+
+    public void goToHomePage(ActionEvent actionEvent) throws IOException {
+        new HomePageFXMLController().goToHomePage(actionEvent);
+    }
 }
